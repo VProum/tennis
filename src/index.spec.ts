60e251a450b101285263cce09ffe5127f5a723eb
Vincent P <vincent.proum@ovrsea.com>
2022-12-11 21:38:52 +0100

    done

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,63 +1,66 @@
-import computeTennisScore, { GameStatus } from "../src/index";
+import computeTennisScore, { createOnGoingGame, Finished, GameStatus, OnGoing } from "../src/index";
 
 test("That's a test!", () => {
   // Given
-  const previousScore: GameStatus = { type: "onGoing", scores: { player1: "0", player2: "0" } };
+  const previousScore: GameStatus = createOnGoingGame({ player1: "0", player2: "0" });
+
+  // When
+  const actualScore = previousScore.score("player1") as OnGoing;
+
+  // Then
+  const expectedScore = { type: "OnGoing", scores: { player1: "15", player2: "0" } };
+  expect({ type: actualScore.type, scores: actualScore.scores }).toEqual(expectedScore);
+});
+test("That's a test!", () => {
+  // Given
+  const previousScore: GameStatus = createOnGoingGame({ player1: "15", player2: "0" });
+
+  // When
+  const actualScore = previousScore.score("player1") as OnGoing;
+
+  // Then
+  const expectedScore = { type: "OnGoing", scores: { player1: "30", player2: "0" } };
+  expect({ type: actualScore.type, scores: actualScore.scores }).toEqual(expectedScore);
+});
+test("That's a test!", () => {
+  // Given
+  const previousScore: GameStatus = { type: "OnGoing", scores: { player1: "30", player2: "0" } };
 
   // When
   const actualScore = computeTennisScore(previousScore, "player1");
 
   // Then
-  const expectedScore: GameStatus = { type: "onGoing", scores: { player1: "15", player2: "0" } };
+  const expectedScore: GameStatus = { type: "OnGoing", scores: { player1: "40", player2: "0" } };
   expect(actualScore).toEqual(expectedScore);
 });
 test("That's a test!", () => {
   // Given
-  const previousScore: GameStatus = { type: "onGoing", scores: { player1: "15", player2: "0" } };
-
-  // When
-  const actualScore = computeTennisScore(previousScore, "player1");
-
-  // Then
-  const expectedScore: GameStatus = { type: "onGoing", scores: { player1: "30", player2: "0" } };
-  expect(actualScore).toEqual(expectedScore);
-});
-test("That's a test!", () => {
-  // Given
-  const previousScore: GameStatus = { type: "onGoing", scores: { player1: "30", player2: "0" } };
-
-  // When
-  const actualScore = computeTennisScore(previousScore, "player1");
-
-  // Then
-  const expectedScore: GameStatus = { type: "onGoing", scores: { player1: "40", player2: "0" } };
-  expect(actualScore).toEqual(expectedScore);
-});
-test("That's a test!", () => {
-  // Given
-  const previousScore: GameStatus = { type: "onGoing", scores: { player1: "30", player2: "0" } };
+  const previousScore: GameStatus = { type: "OnGoing", scores: { player1: "30", player2: "0" } };
 
   // When
   const actualScore = computeTennisScore(previousScore, "player2");
 
   // Then
-  const expectedScore: GameStatus = { type: "onGoing", scores: { player1: "30", player2: "15" } };
+  const expectedScore: GameStatus = { type: "OnGoing", scores: { player1: "30", player2: "15" } };
   expect(actualScore).toEqual(expectedScore);
 });
 test("That's a test!", () => {
   // Given
-  const previousScore: GameStatus = { type: "onGoing", scores: { player1: "40", player2: "0" } };
+  const previousScore: GameStatus = createOnGoingGame({ player1: "0", player2: "0" })
+    .score("player1")
+    .score("player1")
+    .score("player1");
 
   // When
-  const actualScore = computeTennisScore(previousScore, "player1");
+  const actualScore = previousScore.score("player1") as Finished;
 
   // Then
-  const expectedScore: GameStatus = { type: "finished", winner: "player1" };
-  expect(actualScore).toEqual(expectedScore);
+  const expectedScore = { type: "Finished", winner: "player1" };
+  expect({ type: actualScore.type, winner: actualScore.winner }).toEqual(expectedScore);
 });
 test("That's a test!", () => {
   // Given
-  const previousScore: GameStatus = { type: "onGoing", scores: { player1: "40", player2: "30" } };
+  const previousScore: GameStatus = { type: "OnGoing", scores: { player1: "40", player2: "30" } };
 
   // When
   const actualScore = computeTennisScore(previousScore, "player2");
@@ -68,7 +71,7 @@ test("That's a test!", () => {
 });
 test("That's a test!", () => {
   // Given
-  const previousScore: GameStatus = { type: "onGoing", scores: { player1: "30", player2: "40" } };
+  const previousScore: GameStatus = { type: "OnGoing", scores: { player1: "30", player2: "40" } };
 
   // When
   const actualScore = computeTennisScore(previousScore, "player1");
@@ -85,24 +88,24 @@ test("That's a test!", () => {
   const actualScore = computeTennisScore(previousScore, "player1");
 
   // Then
-  const expectedScore: GameStatus = { type: "advantage", advantage: "player1" };
+  const expectedScore: GameStatus = { type: "Advantage", advantage: "player1" };
   expect(actualScore).toEqual(expectedScore);
 });
 
 test("That's a test!", () => {
   // Given
-  const previousScore: GameStatus = { type: "advantage", advantage: "player1" };
+  const previousScore: GameStatus = { type: "Advantage", advantage: "player1" };
 
   // When
   const actualScore = computeTennisScore(previousScore, "player1");
 
   // Then
-  const expectedScore: GameStatus = { type: "finished", winner: "player1" };
+  const expectedScore: GameStatus = { type: "Finished", winner: "player1" };
   expect(actualScore).toEqual(expectedScore);
 });
 test("That's a test!", () => {
   // Given
-  const previousScore: GameStatus = { type: "advantage", advantage: "player1" };
+  const previousScore: GameStatus = { type: "Advantage", advantage: "player1" };
 
   // When
   const actualScore = computeTennisScore(previousScore, "player2");
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-use-before-define */
 type Player = "player1" | "player2";
 
 const SCORES = ["0", "15", "30", "40"] as const;
 type Score = typeof SCORES[number];
 
-export type OnGoing = { scores: Record<Player, Score>; type: "onGoing" };
-export type Finished = { winner: Player; type: "finished" };
-export type Deuce = { type: "Deuce" };
-export type Advantage = { advantage: Player; type: "advantage" };
+type AbstactState<T extends string> = {
+  type: T;
+  score: (scoringPlayer: Player) => GameStatus;
+};
+
+export type OnGoing = AbstactState<"OnGoing"> & { scores: Record<Player, Score> };
+export type Finished = AbstactState<"Finished"> & { winner: Player };
+export type Deuce = AbstactState<"Deuce">;
+export type Advantage = AbstactState<"Advantage"> & { advantage: Player };
 
 export type GameStatus = OnGoing | Finished | Deuce | Advantage;
 
@@ -19,16 +26,66 @@ const extractOtherPlayer = (player: Player): Player => {
   return OTHER_PLAYER[player];
 };
 
+const createDeuceGame = (): Deuce => {
+  return {
+    type: "Deuce",
+    score: (scoringPlayer: Player) => {
+      return createAdvantageGame(scoringPlayer);
+    },
+  };
+};
+const createFinishedGame = (winner: Player): Finished => {
+  return {
+    type: "Finished",
+    winner,
+    score: () => {
+      throw Error("Game is already finished");
+    },
+  };
+};
+
+const createAdvantageGame = (player: Player): Advantage => {
+  return {
+    advantage: player,
+    type: "Advantage",
+    score: (scoringPlayer: Player) => {
+      if (scoringPlayer === player) {
+        return createFinishedGame(scoringPlayer);
+      }
+      return createDeuceGame();
+    },
+  };
+};
+
+export const createOnGoingGame = (scores: Record<Player, Score>): OnGoing => {
+  return {
+    type: "OnGoing",
+    scores,
+    score: (scoringPlayer: Player) => {
+      if (scores[scoringPlayer] === "30" && scores[extractOtherPlayer(scoringPlayer)] === "40") {
+        return createDeuceGame();
+      }
+
+      if (scores[scoringPlayer] === "40") {
+        return createFinishedGame(scoringPlayer);
+      }
+
+      const indexScorePlayer = SCORES.indexOf(scores[scoringPlayer]);
+      return createOnGoingGame({ ...scores, [scoringPlayer]: SCORES[indexScorePlayer + 1] });
+    },
+  };
+};
+
 const computeTennisScore = (previousScore: OnGoing | Deuce | Advantage, scoringPlayer: Player): GameStatus => {
-  if (previousScore.type === "advantage") {
+  if (previousScore.type === "Advantage") {
     if (scoringPlayer === previousScore.advantage) {
-      return { winner: scoringPlayer, type: "finished" };
+      return { winner: scoringPlayer, type: "Finished" };
     }
     return { type: "Deuce" };
   }
 
   if (previousScore.type === "Deuce") {
-    return { advantage: scoringPlayer, type: "advantage" };
+    return { advantage: scoringPlayer, type: "Advantage" };
   }
 
   if (
@@ -39,12 +96,12 @@ const computeTennisScore = (previousScore: OnGoing | Deuce | Advantage, scoringP
   }
 
   if (previousScore.scores[scoringPlayer] === "40") {
-    return { winner: scoringPlayer, type: "finished" };
+    return { winner: scoringPlayer, type: "Finished" };
   }
 
   const indexScorePlayer = SCORES.indexOf(previousScore.scores[scoringPlayer]);
 
-  return { type: "onGoing", scores: { ...previousScore.scores, [scoringPlayer]: SCORES[indexScorePlayer + 1] } };
+  return { type: "OnGoing", scores: { ...previousScore.scores, [scoringPlayer]: SCORES[indexScorePlayer + 1] } };
 };
 
 export default computeTennisScore;
